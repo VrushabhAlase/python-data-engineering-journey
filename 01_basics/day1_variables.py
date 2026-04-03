1f13c404ff02c3e82564bca4ca951ccc948f04af
Vrushabh Alase <vrualase@gmail.com>
2026-04-03 09:49:49 +0530

    Day 1 - Variables and Data Types

---
+++ 01_basics/day1_variables.py
@@ -0,0 +1,14 @@
+# Variables and Data Types
+
+a = 10
+b = 20
+
+name = "Vrushabh"
+is_learning = True
+
+print("Sum:", a + b)
+print("Name:", name)
+print("Is Learning:", is_learning)
+
+print("Type of a:", type(a))
+print("Type of name:", type(name))

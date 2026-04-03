962627050833cd915605c3496a8d54a495f3681c
Vrushabh Alase <vrualase@gmail.com>
2026-04-03 10:18:50 +0530

    Day 1 - Variables completed with practice

--- 01_basics/day1_variables.py
+++ 01_basics/day1_variables.py
@@ -1,5 +1,8 @@
-# Variables and Data Types
+# =========================================
+# Day 1 - Variables and Data Types
+# =========================================
 
+# Basic Variables
 a = 10
 b = 20
 
@@ -12,3 +15,86 @@ print("Is Learning:", is_learning)
 
 print("Type of a:", type(a))
 print("Type of name:", type(name))
+
+
+# =========================================
+# More Variable Examples
+# =========================================
+
+# Integer
+age = 25
+vay = 26
+
+# Float (decimal)
+salary = 50000.75
+pagar = 44000.50
+
+# String
+city = "Pune"
+gaw = "Kolhapur"
+gender = "Male"
+
+# Boolean
+is_working = True
+kam_suru_ahe = True
+
+# Print values
+print("\n--- Variable Values ---")
+print("Age:", age)
+print("Salary:", salary)
+print("City:", city)
+print("Is Working:", is_working)
+
+print("Vay:", vay)
+print("Pagar:", pagar)
+print("Gaw:", gaw)
+print("Kam Suru Ahe:", kam_suru_ahe)
+print("Gender:", gender)
+
+# Print types
+print("\n--- Variable Types ---")
+print(type(age))
+print(type(salary))
+print(type(city))
+print(type(is_working))
+
+print(type(vay))
+print(type(pagar))
+print(type(gaw))
+print(type(kam_suru_ahe))
+
+
+# =========================================
+# Homework Section
+# =========================================
+
+# Task 1: Person Profile
+person_name = "Akshay Patil"
+person_age = 30
+person_city = "Kolhapur"
+person_is_working = True
+person_salary = 1000001.1
+
+print("\n--- Person Details ---")
+print("Person Name:", person_name)
+print("Person Age:", person_age)
+print("Person City:", person_city)
+print("Person Is Working:", person_is_working)
+print("Person Salary:", person_salary)
+
+
+# Task 2: Salary Calculation
+salary = 44000
+bonus = 10000
+
+print("\n--- Salary Calculation ---")
+print("Total Salary:", salary + bonus)
+
+
+# Task 3: Boolean Example
+is_student = False
+is_employee = True
+
+print("\n--- Boolean Check ---")
+print("Am I Earning:", is_employee)
+print("Am I Studying:", is_student)
